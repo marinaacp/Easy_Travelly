782a1bbbdd5b3d6323b08a244ad006f0544b83cb
marina pessoa <marinacp.pessoa@gmail.com>
2022-08-31 16:23:39 -0300

    hotel create metodo feito

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,7 +1,25 @@
 class HotelsController < ApplicationController
+  before_action :set_hotel, only: %i[edit update]
+
   def edit
   end
 
   def update
+    if @hotel.update(hotel_params)
+      redirect_to trip_path(@hotel.trip)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def hotel_params
+    params.permit(:name, :check_in, :check_out, :price, :currency, :category, :zone_name )
+  end
+
+  def set_hotel
+    @hotel = Hotel.find(params[:id])
+    authorize @hotel
   end
 end
