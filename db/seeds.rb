7dd381ec6f3dd68ff7a78ffd109bc3691e57890c
marina pessoa <marinacp.pessoa@gmail.com>
2022-09-06 19:47:46 -0300

    putting more cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,50 +33,52 @@ end
 # To fetch all destinations from the API 7 requests have to be made
 # Fetching only the contries bellow:
 
-# countrycodes = ['US', 'ES', 'BR', 'FR', 'UK', 'PT', 'AR', 'IT', 'DE']
+countrycodes = ['ES', 'BR', 'FR', 'UK', 'PT', 'IT', 'DE']
 
-# countrycodes.each do |code|
-#   url = URI("https://api.test.hotelbeds.com/hotel-content-api/1.0/locations/destinations?fields=countryCode, name&countryCodes=#{code}&language=ENG&from=1001&to=1797&useSecondaryLanguage=false")
+countrycodes.each do |code|
+  url = URI("https://api.test.hotelbeds.com/hotel-content-api/1.0/locations/destinations?fields=countryCode, name&countryCodes=#{code}&language=ENG&from=1001&to=1797&useSecondaryLanguage=false")
 
-#   https = Net::HTTP.new(url.host, url.port)
-#   https.use_ssl = true
-#   request = Net::HTTP::Get.new(url)
-#   request["Api-key"] = ENV['API_KEY']
-#   string = ENV['API_KEY'] + ENV['API_SECRET'] + Time.now.to_i.to_s
-#   hash = Digest::SHA256.hexdigest(string)
-#   request["X-Signature"] = hash
-#   request["Accept"] = "application/json"
-#   response = https.request(request)
-#   response = JSON.parse(response.read_body)
+  https = Net::HTTP.new(url.host, url.port)
+  https.use_ssl = true
+  request = Net::HTTP::Get.new(url)
+  request["Api-key"] = ENV['API_KEY']
+  string = ENV['API_KEY'] + ENV['API_SECRET'] + Time.now.to_i.to_s
+  hash = Digest::SHA256.hexdigest(string)
+  request["X-Signature"] = hash
+  request["Accept"] = "application/json"
+  response = https.request(request)
+  response = JSON.parse(response.read_body)
 
-#   response['destinations'].each do |destination|
-#     puts "Creating destination #{destination['name']['content']}"
-#     new_destination = Destination.create(
-#       code: destination['code'],
-#       name: destination['name']['content'],
-#       country: Country.find_by_code(destination['countryCode'])
-#     )
-#     DestinationAttr.create(
-#       city_name: new_destination.name,
-#       country_name: new_destination.country.name,
-#       city_code: new_destination.code,
-#       country_code: new_destination.country.code
-#     )
-#   end
-# end
+  response['destinations'].each do |destination|
+    new_destination = Destination.create(
+      code: destination['code'],
+      name: destination['name']['content'],
+      country: Country.find_by_code(destination['countryCode'])
+    )
+    puts "Creating destination #{destination['name']['content']} - #{new_destination.country.name}"
+    DestinationAttr.create(
+      city_name: new_destination.name,
+      country_name: new_destination.country.name,
+      city_code: new_destination.code,
+      country_code: new_destination.country.code
+    )
+  end
+  puts "Created destination for #{code}"
+  sleep(30)
+end
 ###############################################################################
 
 # Populating some Destinations
-cities = ['New York City - NYC', 'Los Angeles', 'Boston' , 'Rio de Janeiro', 'Belo Horizonte', 'Sao Paulo', 'Curitiba', 'Brasilia', 'Porto Alegre', 'Fortaleza' , 'Manaus', 'Paris', 'Barcelona', 'Madrid', 'London']
-cities_codes = %w[NYC LAX BOS RIO BHZ SAO CUR BSB POO FTZ MNA PAR BCN MAD LON]
-countries = ["United States", "United States", "United States", "Brazil", "Brazil", "Brazil", "Brazil", "Brazil", "Brazil", "Brazil", "Brazil", "France", "Spain", "Spain", "United Kingdom"]
-countries_codes = %w[US US US BR BR BR BR BR BR BR BR FR ES ES UK]
+# cities = ['New York City - NYC', 'Los Angeles', 'Boston' , 'Rio de Janeiro', 'Belo Horizonte', 'Sao Paulo', 'Curitiba', 'Brasilia', 'Porto Alegre', 'Fortaleza' , 'Manaus', 'Paris', 'Barcelona', 'Madrid', 'London']
+# cities_codes = %w[NYC LAX BOS RIO BHZ SAO CUR BSB POO FTZ MNA PAR BCN MAD LON]
+# countries = ["United States", "United States", "United States", "Brazil", "Brazil", "Brazil", "Brazil", "Brazil", "Brazil", "Brazil", "Brazil", "France", "Spain", "Spain", "United Kingdom"]
+# countries_codes = %w[US US US BR BR BR BR BR BR BR BR FR ES ES UK]
 
-cities.each_with_index do |city, index|
-  DestinationAttr.create(
-          city_name: city,
-          country_name: countries[index],
-          city_code: cities_codes[index],
-          country_code: countries_codes[index]
-        )
-end
+# cities.each_with_index do |city, index|
+#   DestinationAttr.create(
+#           city_name: city,
+#           country_name: countries[index],
+#           city_code: cities_codes[index],
+#           country_code: countries_codes[index]
+#         )
+# end
